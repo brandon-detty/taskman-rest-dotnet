348992514010c29ef1797707b684994c58a38dd0
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2022-12-15 21:14:24 -0800

    pretty-print json in dev env

--- Program.cs
+++ Program.cs
@@ -2,7 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+IMvcBuilder mvcBuilder = builder.Services.AddControllers();
+
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+}
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
